4ebdb2ef9cf8483ec74f6f554357c4108a702b30
kanghy22 <nasmurf1hk@gmail.com>
2017-10-15 17:50:13 -0400

    Checkpoint 12 play/pause: completed

--- scripts/album.js
+++ scripts/album.js
@@ -14,7 +14,7 @@ var albumPicasso = {
      ]
 };
 
-// Another Example Album
+ // Another Example Album
 var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -29,11 +29,26 @@ var albumMarconi = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
 };
+//Assignment album
+var  albumHeartbreak = {
+      title: '808s & Heartbreak',
+      artist: 'Kanye West',
+      label: 'Roc-A-Fella Records',
+      year: '2008',
+      albumArtUrl: 'assets/images/album_covers/18.png',
+      songs: [
+          { title: 'Say You Will', duration: '6:18' },
+          { title: 'Welcome to Heartbreak', duration: '4:23' },
+          { title: 'Heartless', duration: '3:31'},
+          { title: 'Amazing', duration: '3:58' },
+          { title: 'Love Lockdown', duration: '4:31'}
+     ]
+};
 
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -41,15 +56,14 @@ var createSongRow = function(songNumber, songName, songLength) {
 
      return template;
 };
+// #1 make contents global so we can access from window.onload
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-    // #1
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
     // #2
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -64,7 +78,41 @@ var setCurrentAlbum = function(album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
-
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+//we want to add an event listener when user clicks album cover (albumImage)
 window.onload = function() {
+    //default current album when page loads
     setCurrentAlbum(albumPicasso);
+    songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             // Change the content from the number to the play button's HTML
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Revert the content back to the number
+             // Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+    //create an array for the possible album objects
+    var currentAlbum = [albumMarconi, albumHeartbreak, albumPicasso];
+    //default index starts at albumMarconi when clicked
+    var i = 0;
+    //add the event listener
+    albumImage.addEventListener("click", function(foo) {
+        //toggle b/w objects
+        setCurrentAlbum(currentAlbum[i]);
+        i += 1;
+        //if user keeps infinetly clicking
+        if (i == currentAlbum.length) {
+          //reset the index
+          i = 0;
+        }
+    });
 };
